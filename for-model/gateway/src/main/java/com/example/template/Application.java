2589b671e0c2f0db564c0015c464b2ecbae56a32
양성원 <seongwon@yangseong-won-ui-MacBookPro.local>
2024-11-13 23:19:33 +0900

    spring-security

--- for-model/gateway/src/main/java/com/example/template/Application.java
+++ for-model/gateway/src/main/java/com/example/template/Application.java
@@ -1,27 +1,12 @@
 path: /src/main/java/{{options.package}}
 ---
 package {{options.package}};
-{{#if (isSelectedSecurity options.rootModel.toppingPlatforms)}}
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
-
-@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
-public class Application {
-
-    public static ApplicationContext applicationContext;
-    public static void main(String[] args) {
-        applicationContext = SpringApplication.run(Application.class, args);
-    }
-
-
-}
-{{else}}
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.cloud.gateway.filter.headers.XForwardedHeadersFilter;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -33,7 +18,6 @@ public class Application {
 
 
 }
-{{/if}}
 
 
 <function>
