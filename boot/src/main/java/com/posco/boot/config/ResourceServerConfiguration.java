53a4385ac7d21ccafa00bec29e52cb0f36182935
양성원 <seongwon@yangseong-won-ui-MacBookPro.local>
2024-11-16 19:40:57 +0900

    a

---
+++ boot/src/main/java/com/posco/boot/config/ResourceServerConfiguration.java
@@ -0,0 +1,30 @@
+path: {{name}}/s20a01-boot/src/main/java/com/posco/{{name}}/s20a01/config
+fileName: ResourceServerConfiguration.java
+---
+package com.posco.{{name}}.s20a01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .cors()
+            .and()
+            .csrf().disable()
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .oauth2ResourceServer()
+            .jwt();
+    }
+}
