1a073d8d583fffd4881b2f1d9000b098a87bfcd2
양성원 <seongwon@yangseong-won-ui-MacBookPro.local>
2024-11-17 17:07:07 +0900

    oauth mybatis

--- for-model/oauth/src/main/java/com/example/template/repository/UserRepository.java
+++ for-model/oauth/src/main/java/com/example/template/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.template.repository;
 
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import com.example.template.entity.User;
 
