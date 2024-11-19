5a8b7dd35cae0ebbb0e7b021bad3e2f6792d447f
양성원 <seongwon@yangseong-won-ui-MacBookPro.local>
2024-11-19 19:48:56 +0900

    read model

--- service/src/main/java/com/posco/service/AggregateRootRepositoryService.java
+++ service/src/main/java/com/posco/service/AggregateRootRepositoryService.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 {{#attached 'View' this}}
 import com.posco.{{boundedContext.name}}.s20a01.domain.{{aggregate.nameCamelCase}}.mybatis.{{namePascalCase}}MybatisEntity;
-import com.posco.{{boundedContext.name}}.s20a01.domain.{{aggregate.nameCamelCase}}.mybatis.{{namePascalCase}}Mapper;
+import com.posco.{{boundedContext.name}}.s20a01.domain.{{aggregate.nameCamelCase}}.mybatis.{{../namePascalCase}}Mapper;
 {{/attached}}
 {{#commands}}
 import com.posco.{{boundedContext.name}}.s20a01.domain.{{../nameCamelCase}}.{{namePascalCase}}Command;
