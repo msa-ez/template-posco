90aa981797edd4857767187c92be7f412b37e3d6
양성원 <seongwon@yangseong-won-ui-MacBookPro.local>
2024-11-19 19:30:41 +0900

    read model

--- domain/src/main/java/com/posco/domain/AggregateRoot.java
+++ domain/src/main/java/com/posco/domain/AggregateRoot.java
@@ -6,7 +6,7 @@ path: {{boundedContext.name}}/s20a01-domain/src/main/java/com/posco/{{boundedCon
 package com.posco.{{boundedContext.name}}.s20a01.domain.{{nameCamelCase}};
 
 {{#aggregateRoot.fieldDescriptors}}{{#isVO}}import com.posco.{{../boundedContext.name}}.s20a01.domain.{{namePascalCase}};{{/isVO}}{{/aggregateRoot.fieldDescriptors}}
-{{#aggregateRoot.fieldDescriptors}}{{#isPrimitive className}}import com.posco.{{../boundedContext.name}}.s20a01.domain.{{namePascalCase}};{{/isPrimitive}}{{/aggregateRoot.fieldDescriptors}}
+{{#aggregateRoot.fieldDescriptors}}{{#if (isPrimitive className)}}import com.posco.{{../boundedContext.name}}.s20a01.domain.{{namePascalCase}};{{/if}}{{/aggregateRoot.fieldDescriptors}}
 
 import javax.persistence.*;
 import java.util.List;
