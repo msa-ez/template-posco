dc801104dc5a160287bea6ac9205fd535c20383c
kyusoo <rbtn110@uengine.org>
2024-11-26 12:58:31 +0900

    enum

--- for-model/common/js/Aggregate.js
+++ for-model/common/js/Aggregate.js
@@ -9,18 +9,8 @@ $(document).ready(function(){
       };
     var OPT = {
         "Cols": [
-    {"Type": "Text","Name": "TextData","Width": 100,"Align": "Center","CanEdit": 1},
-    {"Header": "줄넘김문자열(Lines)","Type": "Lines","Name": "LinesData","MinWidth": 250,"Align": "Center","CanEdit": 1,"RelWidth": 1},
     {"Type": "Enum","Name": "ComboData","Align": "Center","Enum": "|대기|진행중|완료","EnumKeys": "|01|02|03"},
-    {"Header": "버튼(Button)","Type": "Button","Name": "ISO","Width": 80,"Align": "Left","CanEdit": 0,"Button": "Button"},
-    {"Header": "정수(Int)","Type": "Int","Name": "IntData","Width": 80,"Align": "Right","CanEdit": 1},
-    {"Header": "실수(Float)","Type": "Float","Name": "FloatData","Format": "#,##0.0#","Width": 80,"Align": "Right","CanEdit": 1},
     {"Header": "날짜(Date)","Type": "Date","Name": "DateData","Width": 150,"Align": "Center","CanEdit": 1,"EmptyValue": "날짜를 입력해주세요"},
-    {"Header": "앵커(Link)","Type": "Link","Name": "LinkData","TextStyle": 4,"TextColor": "#0000FF","Width": 60,"CanEdit": 0},
-    {"Header": "이미지(Img)","Type": "Img","Name": "ImageData","Width": 70,"Align": "Center","CanEdit": 1},
-    {"Header": "패스워드(Pass)","Type": "Pass","Name": "PassData","Width": 80,"Align": "Left","CanEdit": 1},
-    {"Header": "라디오(Radio)","Type": "Radio","Name": "RadioData","Width": 140,"Align": "Center","CanEdit": 1,"Enum": "|상|중|하","EnumKeys": "|H:1|M:1|L:1"},
-    {"Header": {"Value": "체크박스(Bool)","HeaderCheck": 1},"Type": "Bool","Name": "CheckData","Width": 110,"Align": "Center","CanEdit": 1}
   ]
         Cols:[
             {{#aggregateRoot}}
@@ -152,7 +142,7 @@ window.$HandleBars.registerHelper('checkEnum', function (type, field) {
         if(type == relation[i].targetElement.name){
             var items = relation[i].targetElement.items;
             var result = items.map(item => item.value).join('|');
-            return `"${result}"`;
+            return `"|${result}"`;
         }
     }
 });
