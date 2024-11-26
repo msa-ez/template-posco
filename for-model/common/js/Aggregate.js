b8dbfefca111958de16edcd3d54240ebbca131f7
kyusoo <rbtn110@uengine.org>
2024-11-26 15:27:26 +0900

    vo handlebar추가

--- for-model/common/js/Aggregate.js
+++ for-model/common/js/Aggregate.js
@@ -167,7 +167,7 @@ window.$HandleBars.registerHelper('createVoField', function (type, field) {
             if(vo && vo.fieldDescriptors){
                 for(var j = 0; j < vo.fieldDescriptors.length; j++){
                     var voField = vo.fieldDescriptors[j];
-                    result.push({"Header": ["${vo.namePascalCase}", "${voField.nameCamelCase}"], "Name": "${voField.namePascalCase}", "Width": 110});
+                    result.push(`{"Header": ["${vo.namePascalCase}", "${voField.nameCamelCase}"], "Name": "${voField.namePascalCase}", "Width": 110}`);
                 }
             }else{
                 return;
