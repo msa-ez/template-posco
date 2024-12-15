afbcb2d75ed41f8291dbaffb1db1541e93639fea
kyusoo <rbtn110@uengine.org>
2024-12-15 17:01:47 +0900

    vo내부enum

--- for-model/common/js/Aggregate.js
+++ for-model/common/js/Aggregate.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
             {{#entities.relations}}
             {{#if targetElement.fieldDescriptors}}
             {{#targetElement.fieldDescriptors}}
-            {"Header": ["{{../nameCamelCase}}", "{{nameCamelCase}}"], "Name": "{{nameCamelCase}}", "Type":, "Width": 140, "CanEdit": 1},
+            {"Header": ["{{../targetElement.nameCamelCase}}", "{{nameCamelCase}}"], "Name": "{{nameCamelCase}}", "Type":, "Width": 140, "CanEdit": 1},
             {{/targetElement.fieldDescriptors}}
             {{/if}}
             {{/entities.relations}}
