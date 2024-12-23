d06b9d893317bc402c19effb827f05d29e89425d
kyusoo <rbtn110@uengine.org>
2024-12-23 11:57:30 +0900

    update html 추가

--- for-model/common/js/Aggregate.js
+++ for-model/common/js/Aggregate.js
@@ -162,7 +162,7 @@ function update(data){
     {{#fieldDescriptors}}{{#if isKey}}var id = data.{{nameCamelCase}}{{/if}}{{/fieldDescriptors}}
     $.ajax({
         url: `/{{aggregate.namePlural}}/${id}`,
-        method: "{{restRepository.method}}",
+        method: "{{restRepositoryInfo.method}}",
         contentType: "application/json",
         data: JSON.stringify(data),
         success: function() {
